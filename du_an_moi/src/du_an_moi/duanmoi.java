84a90935e83243d1c04d795129d39ff10f0db3ab
longhien0510 <hiennguyen05102005@gmail.com>
2025-04-09 11:35:48 +0700

    cr7

---
+++ du_an_moi/src/du_an_moi/duanmoi.java
@@ -0,0 +1,11 @@
+package du_an_moi;
+
+public class duanmoi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+		System.out.println("tao la "+a + "gio ");
+	}
+
+}
